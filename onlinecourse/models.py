7de39d0d0038abdba2d763f5fb9e144cf2c9fda2
wintercode <kenfackjordanjunior@gmail.com>
2026-03-14 12:38:41 +0100

    task1 completed

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,10 +94,23 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+   
+class Question(models.Model): 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.content
+    
+class Choice(models.Model): 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    is_correct = models.BooleanField(default=False)
 
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
